552a1cef71cf9058ec35db0fcdb252aae41ede80
yushin.kim <yushin@aptner.com>
2024-12-17 12:44:17 +0900

    init

---
+++ src/test/java/com/aptner/point/designpatternexample/DesignPatternExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aptner.point.designpatternexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
